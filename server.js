2bee46aa545f6a79f6688c7b4c194e1f05f25abc
Hitesh OnFit <hiteshmishra708@gmail.com>
2023-08-21 18:02:42 +0000

    fix

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const directory = './dist'; // Replace this with your actual directory path
 
 app.use(express.static(directory));
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(3001, () => {
+  console.log('Server is running on port 3001');
 });
