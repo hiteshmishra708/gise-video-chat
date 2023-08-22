bc1b113404bfadab750b772aec6f719952bf6150
Hitesh Mishra <hiteshmishra708@gmail.com>
2023-08-22 10:45:03 +0530

    fix

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const cors = require('cors')
+
 const app = express();
+
+// enabling CORS for some specific origins only.
+let corsOptions = {
+  origin : ['http://localhost:3000'],
+}
+ 
+app.use(cors(corsOptions))
 
 const directory = './dist'; // Replace this with your actual directory path
 
